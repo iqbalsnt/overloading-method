f6d49aace99b304c649e36b26c05be16e5f98463
iqbalsnt <iqbalsnt@students.amikom.ac.id>
2021-03-31 20:20:25 +0700

    latihanmethod

---
+++ LatihanMethod/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LatihanMethod
+{
+    public class Calculator
+    {
+        public int penjumlahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        // overload method
+        public int penjumlahan(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public int pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        // method static
+        public static int perkalian(int a, int b )
+        {
+            return a * b;
+        }
+    }
+}
---
+++ LatihanMethod/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LatihanMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklarasi variable
+            int hasil;
+
+            static void Cetakhasil(string hasil)
+            {
+                Console.WriteLine(hasil);
+            }
+
+            // membuat objek dari class calculator
+            Calculator calculator = new Calculator();
+
+            // panggil method penjumlah
+            hasil = calculator.penjumlahan(10, 2);
+            Cetakhasil(" hasil penjumlahan : " + hasil);
+
+            // panggil method penjumlahan dengan 3 parameter ( overload method )
+            hasil = calculator.penjumlahan(10, 2, 3);
+            Cetakhasil(" hasil penjumlahan overload : " + hasil);
+
+            // panggil method pengurangan
+            hasil = calculator.pengurangan(7, 2);
+            Cetakhasil(" hasil pengurangan : " + hasil);
+
+            // panggil langsung static method langsung dari nama classnya
+            hasil = Calculator.perkalian(5, 2);
+            Cetakhasil(" hasil perkalian : " + hasil);
+            Console.ReadKey();
+        }
+    }
+}
